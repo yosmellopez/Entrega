ea6a2c866ad7d9acca26e196bb7145764f20bb3f
yosme <yosmellopez@gmail.com>
2018-11-05 13:47:51 -0500

    Primer commit

---
+++ src/main/java/com/ult/entrega/EntregaApplication.java
@@ -0,0 +1,12 @@
+package com.ult.entrega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntregaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntregaApplication.class, args);
+    }
+}
