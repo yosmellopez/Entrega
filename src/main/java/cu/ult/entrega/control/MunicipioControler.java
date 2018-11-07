b4d8f732907826d354e5ff33e25e912411d7e768
Yosmel López Pimentel <yosmellp@ult.edu.cu>
2018-11-07 12:32:54 -0500

    Actualizacion Controladores

--- src/main/java/cu/ult/entrega/control/MunicipioControler.java
+++ src/main/java/cu/ult/entrega/control/MunicipioControler.java
@@ -6,91 +6,84 @@
 package cu.ult.entrega.control;
 
 import cu.ult.entrega.clases.Municipio;
-import cu.ult.entrega.excepcion.ProvinciaException;
+import cu.ult.entrega.excepcion.MunicipioException;
 import cu.ult.entrega.repositorio.MunicipioRepositorio;
-import cu.ult.entrega.repositorio.ProvinciaRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 import javax.persistence.EntityNotFoundException;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import javax.validation.Valid;
 import java.util.List;
+import java.util.Locale;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static cu.ult.entrega.control.AppResponse.failure;
+import static cu.ult.entrega.control.AppResponse.success;
 
 
-@Controller
+@RestController
+@RequestMapping("/api")
 public class MunicipioControler {
 
     @Autowired
-    MunicipioRepositorio municipioRepositorio;
+    MunicipioRepositorio MunicipioRepositorio;
 
-    @Autowired
-    ProvinciaRepositorio provinciaRepositorio;
-
-    @RequestMapping(value = "/municipio")
-    public ModelAndView listarMunicipios(ModelMap map) {
-        List<Municipio> municipios = municipioRepositorio.findAll();
-        map.put("municipios", municipios);
-        map.put("cant", municipios.size());
-
-        if (municipios.isEmpty()) {
-            map.put("response", HttpStatus.NO_CONTENT);
-            return new ModelAndView(new MappingJackson2JsonView(), map);
-        }
-
-        map.put("response", HttpStatus.OK);
-        return new ModelAndView(new MappingJackson2JsonView(), map);
+    @RequestMapping(value = "/Municipio")
+    public ResponseEntity<AppResponse<Municipio>> listarMunicipios(Pageable p) {
+        Page<Municipio> page = MunicipioRepositorio.findAll(p);
+        List<Municipio> Municipios = page.getContent();
+        return ResponseEntity.ok(success(Municipios).total(page.getTotalElements()).build());
     }
 
-    /**
-     * @param municipio
-     * @param result
-     * @return
-     */
-    @PostMapping(value = "/municipio")
-    public ResponseEntity<Municipio> insertarMunicipio(@RequestBody Municipio municipio, BindingResult result) {
-        municipioRepositorio.saveAndFlush(municipio);
-        return ResponseEntity.ok(municipio);
+    @PostMapping(value = "/Municipio/nueva")
+    public ResponseEntity<AppResponse<Municipio>> insertarMunicipio(@Valid @RequestBody Municipio Municipio) {
+        MunicipioRepositorio.saveAndFlush(Municipio);
+        return ResponseEntity.ok(success(Municipio).build());
     }
 
-    @PutMapping(value = "/municipio/{id}")
-    public ResponseEntity<Municipio> updateMunicipio(@PathVariable("id") Optional<Municipio> optional, @RequestBody Municipio municipio) {
-        Municipio currentMunicipio = optional.orElseThrow(() -> new EntityNotFoundException("Municipio no encontrado"));
-        currentMunicipio.setCodigo(municipio.getCodigo());
-        currentMunicipio.setNombre(municipio.getNombre());
-        currentMunicipio.setProvincia(municipio.getProvincia());
-
-        municipioRepositorio.saveAndFlush(currentMunicipio);
-        return new ResponseEntity<Municipio>(currentMunicipio, HttpStatus.OK);
+    @PutMapping(value = "/Municipio/{id}")
+    public ResponseEntity<AppResponse<Municipio>> actualizarMunicipio(@PathVariable("id") Optional<Municipio> optional, @Valid @RequestBody Municipio Municipio) {
+        Municipio currentProvinc = optional.orElseThrow(() -> new EntityNotFoundException("Municipio no encontrado"));
+        currentProvinc.setCodigo(Municipio.getCodigo());
+        currentProvinc.setNombre(Municipio.getNombre());
+        MunicipioRepositorio.saveAndFlush(currentProvinc);
+        return ResponseEntity.ok(success(Municipio).build());
     }
 
-    @RequestMapping(value = "/municipio/{id}", method = RequestMethod.DELETE)
-    public ResponseEntity<Municipio> deleteMunicipio(@PathVariable("id") long id) {
-        System.out.println("Fetching & Deleting User with id " + id);
-
-        Municipio municipio = municipioRepositorio.findById(id).orElseThrow(() -> new EntityNotFoundException("Municipio no encontrado"));
-        if (municipio == null) {
-            System.out.println("Unable to delete. User with id " + id + " not found");
-            return new ResponseEntity<Municipio>(HttpStatus.NOT_FOUND);
-        }
-
-        municipioRepositorio.deleteById(id);
-        return new ResponseEntity<Municipio>(HttpStatus.NO_CONTENT);
+    @DeleteMapping(value = "/Municipio/{id}")
+    public ResponseEntity<AppResponse> deleteMunicipio(@PathVariable("id") Optional<Municipio> optional) {
+        Municipio Municipio = optional.orElseThrow(() -> new EntityNotFoundException("Provinca no encontrada."));
+        MunicipioRepositorio.delete(Municipio);
+        return ResponseEntity.ok(AppResponse.success("Municipio eliminada exitosamente.").build());
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<AppResponse> tratarValidacion(MethodArgumentNotValidException ex, Locale locale) {
+        List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
+        String mensaje = fieldErrors.parallelStream().map(error -> error.getDefaultMessage()).collect(Collectors.joining(", "));
+        return ResponseEntity.ok(failure(mensaje).build());
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<AppResponse> tratarValidacion(ConstraintViolationException ex, Locale locale) {
+        Set<ConstraintViolation<?>> violations = ex.getConstraintViolations();
+        String mensaje = violations.parallelStream().map(error -> error.getMessage()).collect(Collectors.joining(", "));
+        return ResponseEntity.ok(failure(mensaje).build());
+    }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<AppResponse> mostrarError(Exception e) {
-        ProvinciaException generalException = new ProvinciaException(e);
+        MunicipioException generalException = new MunicipioException(e);
         return ResponseEntity.ok(failure(generalException.tratarExcepcion()).build());
     }
 
---
+++ src/main/java/cu/ult/entrega/excepcion/MunicipioException.java
@@ -0,0 +1,8 @@
+package cu.ult.entrega.excepcion;
+
+public class MunicipioException extends GeneralException{
+
+    public MunicipioException(Throwable cause) {
+        super(cause);
+    }
+}
