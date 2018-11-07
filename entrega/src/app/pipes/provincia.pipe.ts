c3d748be9c53f540b816d44e1c0e31c3487f7988
Yosmel López Pimentel <yosmellp@ult.edu.cu>
2018-11-07 13:19:24 -0500

    Actualizacion Municipio Angular

--- entrega/src/app/pipes/provincia.pipe.ts
+++ entrega/src/app/pipes/provincia.pipe.ts
@@ -1,12 +1,14 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import {Pipe, PipeTransform} from '@angular/core';
+import {Provincia} from "../modelo";
 
 @Pipe({
-  name: 'provincia'
+    name: 'provincia'
 })
 export class ProvinciaPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
-  }
-
+    transform(value: Provincia, args?: any): any {
+        if (value)
+            return value.nombre;
+        return "";
+    }
 }
