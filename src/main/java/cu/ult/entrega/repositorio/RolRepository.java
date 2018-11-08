51378f8c0f70e932d285bc71fed8a00129be0de6
Yosmel López Pimentel <yosmellp@ult.edu.cu>
2018-11-08 12:58:23 -0500

    Actualizacion Completa.

--- src/main/java/cu/ult/entrega/repositorio/RolRepository.java
+++ src/main/java/cu/ult/entrega/repositorio/RolRepository.java
@@ -1,15 +1,7 @@
 package cu.ult.entrega.repositorio;
 
 import cu.ult.entrega.clases.Rol;
-import cu.ult.entrega.clases.Usuario;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-import java.util.Optional;
 
 public interface RolRepository extends JpaRepository<Rol, Integer> {
 
--- src/main/java/cu/ult/entrega/repositorio/UsuarioRepository.java
+++ src/main/java/cu/ult/entrega/repositorio/UsuarioRepository.java
@@ -2,19 +2,14 @@ package cu.ult.entrega.repositorio;
 
 import cu.ult.entrega.clases.Usuario;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
     String USERS_BY_LOGIN_CACHE = "usersByLogin";
 
-    @EntityGraph(attributePaths = "comentarios")
     @Cacheable(cacheNames = USERS_BY_LOGIN_CACHE)
     Optional<Usuario> findOneByUsername(String username);
 }
