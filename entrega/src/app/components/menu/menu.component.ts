dddd2753cbf7c1c6d4bacdb6637229489eed41bf
Pablo Caram <pablocaramsantiesteban@gmail.com>
2018-11-19 05:58:49 +0100

    Estos son los cambios que hice

--- entrega/src/app/components/menu/menu.component.ts
+++ entrega/src/app/components/menu/menu.component.ts
@@ -1,7 +1,7 @@
 import {Component, ElementRef, HostListener, Input, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'cdk-menu',
+  selector: 'menuIsquierda',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
